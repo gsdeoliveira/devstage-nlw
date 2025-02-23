9522899c6ea3ce25c26ffa084b5234084dbe718c
gsdeoliveira <gabriielsousa2002@gmail.com>
2025-02-23 07:32:18 -0300

    feat: organize folders

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,7 @@
 import Logo from '@/assets/logo.svg'
-import { Button } from '@/components/button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { ArrowRight, Mail, Radio, User } from 'lucide-react'
+import { Radio } from 'lucide-react'
 import Image from 'next/image'
+import { SubscriptionForm } from './subscription-form'
 
 export default function Home() {
   return (
@@ -38,35 +37,7 @@ export default function Home() {
           </p>
         </div>
 
-        <form
-          action=""
-          className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"
-        >
-          <h2 className="font-heading font-semibold text-gray-200 text-xl">
-            Inscrição
-          </h2>
-
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="text" placeholder="Nome completo" />
-            </InputRoot>
-
-            <InputRoot>
-              <InputIcon>
-                <Mail />
-              </InputIcon>
-              <InputField type="email" placeholder="Email" />
-            </InputRoot>
-          </div>
-
-          <Button type="submit">
-            Confirmar
-            <ArrowRight />
-          </Button>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
   )
